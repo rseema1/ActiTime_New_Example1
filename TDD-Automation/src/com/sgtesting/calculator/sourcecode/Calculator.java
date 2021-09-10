1718b45f963f3af0e7746bfede6baa3e338e5023
rseema1 <rseemashetty.1@gmail.com>
2021-09-10 20:29:04 +0530

    TDD Framewok

---
+++ TDD-Automation/src/com/sgtesting/calculator/sourcecode/Calculator.java
@@ -0,0 +1,24 @@
+package com.sgtesting.calculator.sourcecode;
+
+public class Calculator {
+	
+	/**
+	 * Author:
+	 * Purpose:
+	 * Description:
+	 */
+	public int addition(int num1,int num2)
+	{
+		return (num1+num2);
+	}
+	
+	/**
+	 * Author:
+	 * Purpose:
+	 * Description:
+	 */
+	public int multiplication(int num1,int num2)
+	{
+		return (num1*num2);
+	}
+}
